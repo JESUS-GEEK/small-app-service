dd3acfcb0c475ff56e78307ec35e44ad6b8c5616
wjyu <wjyu@iflytek.com>
2020-08-21 17:41:40 +0800

    style:地图tab-icon

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,23 +13,25 @@ Page({
   data: {
     latitude:'',
     longitude:'',
-    polyline: [{
-      points:[{
-          longitude: 117.129045,
-          latitude:31.825284
-        },
-        {
-          longitude: 117.150438,
-          latitude:31.837456
-        },{
-          longitude:117.165134,
-          latitude:31.870335
-        },
-      ],
-      color: "orange",
-      width: 20,
-      dottedLine: true,
-    }],
+    points: [],
+    polyline: [],
+    // polyline: [{
+    //   points:[{
+    //       longitude: 117.129045,
+    //       latitude:31.825284
+    //     },
+    //     {
+    //       longitude: 117.150438,
+    //       latitude:31.837456
+    //     },{
+    //       longitude:117.165134,
+    //       latitude:31.870335
+    //     },
+    //   ],
+    //   color: "orange",
+    //   width: 20,
+    //   dottedLine: true,
+    // }],
     markers:[],
     show:false,
     activeName: [],
@@ -57,16 +59,40 @@ Page({
         title: "线路图"
       })
     }
+    var temp = [{
+      longitude: 117.129045,
+      latitude:31.825284
+    },
+    {
+      longitude: 117.150438,
+      latitude:31.837456
+    },{
+      longitude:117.165134,
+      latitude:31.870335
+    },
+  ]
+  var polyline = [{
+    color: "orange",
+    width: 20,
+    dottedLine: true,
+    points:temp,  
+  }]
+  this.setData({
+    polyline:polyline,
+    points:temp
+  })
       wx.getLocation({
         type: 'wgs84', //wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
         success: (res) => {
           this.setData({
             latitude : res.latitude,
             longitude : res.longitude,
+           
             // polyline:this.polyline
           })
         },      
       })
+    
 
   },
   onShow: function(){
